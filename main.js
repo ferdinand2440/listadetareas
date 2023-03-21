1cde62c7f6c13b1f0f1d6fbe58fa652df3ef147b
Fernando <ferdinand.sayago@gmail.com>
2023-03-21 18:48:16 -0300

    Agregué botón historial

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ const empty = document.querySelector(".empty");
 const events = [];
 const historyDiv = document.querySelector("#historial");
 
-addBtn.addEventListener("click", (e) => {
 
+addBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    
 
     const text = input.value;
-
+    
     if(text !== ""){
        
         const li = document.createElement("li");
@@ -55,14 +54,55 @@ function addDeleteBtn (){
 
 }
 
-function displayHistory() {
-    historyDiv.innerHTML = "HISTORIAL";
 
-    events.forEach((event) => {
-        const p = document.createElement ("p");
+function displayHistory() {
+    historyDiv.innerHTML = "<span style='color: dark; font-weight: bold; font-size: 20px;'>HISTORIAL</span>";
+
+    const lastFiveEvents = events.slice(-5); // get the last five events in the array
+
+    lastFiveEvents.forEach((event) => {
+        const p = document.createElement("p");
         p.textContent = event;
         historyDiv.appendChild(p);
     });
-    
+
+    const viewAllBtn = document.createElement("button"); // create a view all button
+    viewAllBtn.textContent = "Ver todo";
+    viewAllBtn.className = "btn-view-all";
+
+    viewAllBtn.addEventListener("click", (e) => {
+        historyDiv.innerHTML = "<span style='color: dark; font-weight: bold; font-size: 20px;'>HISTORIAL (todas)</span>";
+
+
+        events.forEach((event) => {
+            const p = document.createElement("p");
+            p.textContent = event;
+            historyDiv.appendChild(p);
+        });
+        historyDiv.appendChild(closeBtn); // add a close button to the history list
+    });
+
+    const closeBtn = document.createElement("button"); // create a close button
+    closeBtn.textContent = "Cerrar";
+    closeBtn.className = "btn-close";
+
+    closeBtn.addEventListener("click", (e) => {
+        historyDiv.innerHTML = "<span style='color: dark; font-weight: bold; font-size: 20px;'>HISTORIAL (últimas 5 tareas)</span>";
+        
+        lastFiveEvents.forEach((event) => {
+            const p = document.createElement("p");
+            p.textContent = event;
+            historyDiv.appendChild(p);
+        });
+        historyDiv.appendChild(viewAllBtn); // add a view all button to the history list
+    });
+
+    historyDiv.appendChild(viewAllBtn); // add the view all button to the history list
 }
+
+
+
+
+
+
 
